1d7662f6aedcae0923e2adcc176289d5091159b9
piyushpratapsingh-exe <singhpiyush9666@gmail.com>
2026-02-25 15:11:07 +0530

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
 android {
@@ -71,7 +70,7 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
     // Coil (Image loading)
     implementation("io.coil-kt:coil-compose:2.6.0")
