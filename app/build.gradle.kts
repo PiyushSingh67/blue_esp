b2a325c26f8d548e7522aa3771e638d47126e2c5
theaniketraj <cs2258.diatm@gmail.com>
2026-03-07 13:02:15 +0530

    Update: Upgrade Room library version to 2.8.4 to fix compatibility with KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Room
-    val room_version = "2.6.1"
+    val room_version = "2.8.4"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
